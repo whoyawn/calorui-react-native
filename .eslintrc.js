7be74f0def609ce17602c02840ba1971bc49c031
Huyanh Hoang <huyanhh@uci.edu>
2017-02-23 16:30:48 -0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "rules": {
+    "no-use-before-define": 0, // disallow use of variables before they are defined
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "react/sort-comp": 0,
+    "no-underscore-dangle": 0,
+  }
+};
